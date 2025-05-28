430b354d170aba81cedc94b698e3d3146fb3ce2d
MugiD <bekslambek22@gmail.com>
2025-05-28 23:58:43 +0500

    feat:add personal information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,102 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const projects = [
+    {
+      name: "distr",
+      description:
+        "Generates distribution charts and tables based on input information. Currently involves bynomial and puasson distributions.",
+      href: "https://problify.vercel.app",
+      dates: "2024",
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <main className="flex flex-col min-h-screen mx-auto max-w-2xl px-6 py-12">
+      <div className="flex flex-row justify-between items-center mt-12 mb-4">
+        <div className="flex flex-row items-center gap-0.5">
+          <Image width={36} height={36} src="favicon.svg" alt="icon image" />
+          <h1 className="font-bold">Bek Slambek</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-row justify-between gap-3">
+          <Link
+            href="./resume.pdf"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="hover:underline underline-offset-2"
+          >
+            /resume
+          </Link>{" "}
+          <Link
+            href="https://github.com/mugid"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="hover:underline underline-offset-2"
+          >
+            /github
+          </Link>{" "}
+          <Link
+            href="https://linkedin.com/in/sbek22"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="hover:underline underline-offset-2"
+          >
+            /linkedin
+          </Link>
+        </div>
+      </div>
+      <div className="mb-12 flex flex-row gap-4">
+        <div className="flex-1 leading-snug space-y-4 bg-[#2b2b2b] p-4 rounded-lg">
+          <p>
+            I&apos;m a frontend developer and aspiring designer from Almaty,
+            Kazakhstan. I use{" "}
+            <span className="text-[#fff3b0]">
+              React, Next.js, and Tailwind CSS
+            </span>{" "}
+            to build clean, responsive web apps — often blending development
+            with design.
+          </p>
+          <p>
+            I&apos;m passionate about learning fast, building things that people
+            actually use, and creating experiences that feel intuitive and
+            thoughtful.
+          </p>
+          <p>
+            If you want to explore the design I do, check it out{" "}
+            <Link
+              href="https://comingsoon.com"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="hover:underline underline-offset-2 text-[#fff3b0]"
+            >
+              here
+            </Link>
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-row justify-end items-center gap-0.5">
+        <Image width={36} height={36} src="favicon.svg" alt="icon image" />
+        <h1 className="font-bold">Projects</h1>
+      </div>
+      <div className="mb-6 space-y-4 leading-snug bg-[#2b2b2b] p-4 rounded-lg">
+        {projects.map((p, id) => (
+          <div key={id} className="flex flex-col gap-3">
+            <div className="flex justify-between items-center">
+              <a
+                href={p.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-bold hover:underline underline-offset-2"
+              >
+                {p.name}
+              </a>
+              <span className="text-sm font-mono">{p.dates}</span>
+            </div>
+            <p>{p.description}</p>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
