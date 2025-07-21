db1f409903f59dab83533236079ffb96304b6c68
mugid <bekslambek22@gmail.com>
2025-07-21 22:06:32 +0500

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
 import Profile from "@/components/profile";
 import About from "@/components/about";
-import Projects from "@/components/projects"
+import Skills from "@/components/skills";
+import Projects from "@/components/projects";
 
 export default function Home() {
-
   return (
     <main className="min-h-screen mx-auto max-w-2xl px-10">
       <Profile />
       <About />
+      <Skills />
       <Projects />
+      <div className="my-12">
+        <p className="text-center">
+          contact me via telegram:{" "}
+          <a
+            href="https://t.me/wanderbeck"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-primary hover:font-semibold transition-all"
+          >
+            @wanderbeck
+          </a>
+        </p>
+      </div>
     </main>
   );
 }
