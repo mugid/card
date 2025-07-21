3787592f226f38a53f7811b3ae409bcb37166da0
mugid <bekslambek22@gmail.com>
2025-07-21 22:52:40 +0500

    feat: change layout

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Profile() {
   return (
-    <div className="flex md:flex-row flex-col-reverse items-start justify-between md:items-center mt-12 pt-12">
+    <div className="flex md:flex-row flex-col-reverse items-start justify-between md:items-end mt-12 pt-12">
       <div className="flex flex-col items-start justify-center gap-1">
         <h1 className="font-bold md:text-4xl text-xl text-primary">Bek Slambek</h1>
         <p className="text-md font-semibold italic text-foreground/50">design & build</p>
       </div>
-      <div className="flex flex-row justify-between gap-3">
+      <div className="flex flex-row justify-between gap-x-3">
         <Link
           href="./resume.pdf"
           rel="noopener noreferrer"
