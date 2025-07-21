49786e80e30116aa7112097518be71960bedc9d1
mugid <bekslambek22@gmail.com>
2025-07-21 21:29:37 +0500

    feat: add interactivity to projects card

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Projects() {
   const projects = [
@@ -6,48 +6,44 @@ export default function Projects() {
       name: "keyano",
       description: "An audio recorder for piano generated music.",
       href: "https://keyano.vercel.app",
-      dates: "2025",
     },
     {
       name: "distr.",
       description:
         "Generates distribution charts and tables based on input information. Currently involves bynomial and poisson distributions.",
       href: "https://distr.vercel.app",
-      dates: "2024",
     },
     {
       name: "feynapp",
       description:
         "App for an effective learning based on Feynmann technique with an implementation of AI",
       href: "https://github.com/mugid/feynapp",
-      dates: "2023",
     },
   ];
 
   return (
-    <div className='mt-12'>
+    <div className="mt-12">
       <div className="flex flex-row justify-start items-center gap-1 mb-4">
         <Image width={24} height={24} src="icon.svg" alt="icon image" />
         <h1 className="font-semibold text-xl">Projects</h1>
       </div>
-      <div className="mb-6 space-y-4 leading-snug">
+      <div className="leading-snug md:columns-2 ">
         {projects.map((p, id) => (
           <div
             key={id}
-            className="flex flex-col gap-3 bg-[#2b2b2b] p-4 rounded-lg"
+            className="group relative flex flex-col justify-end gap-2 h-min bg-[#181818] p-4 pt-25 mb-4 rounded-lg overflow-hidden"
           >
-            <div className="flex justify-between items-center">
-              <a
-                href={p.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-bold hover:underline underline-offset-2"
-              >
-                {p.name}
-              </a>
-              <span className="text-sm font-mono">{p.dates}</span>
-            </div>
-            <p>{p.description}</p>
+            <h1 className="font-semibold">{p.name}</h1>
+            <p className="text-foreground/60">{p.description}</p>
+
+            <a
+              href={p.href}
+              target="_blank"
+              rel="noreferrer"
+              className="absolute top-[100%] h-full w-full bg-[#181818]/80 rounded-lg group-hover:top-0 transition-all flex items-center justify-center"
+            >
+             <span className="text-primary italic">view more...</span>
+            </a>
           </div>
         ))}
       </div>
