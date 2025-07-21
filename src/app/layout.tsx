7126833d6a625aaaad188c8594390d381f35c82c
mugid <bekslambek22@gmail.com>
2025-07-21 19:34:35 +0500

    feat: change color scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,13 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
-
-
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans}.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans}.variable} ${geistMono.variable} font-sans antialiased`}
       >
         {children}
       </body>
